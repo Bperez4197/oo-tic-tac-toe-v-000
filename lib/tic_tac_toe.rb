42e9344c26e0daedc8284da9f823f98e2b028ebf
Bperez4197 <bryceperez31@gmail.com>
2017-11-07 05:54:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,6 @@
 class TicTacToe
    def Initialize(board)
-     board = [" "," "," "," "," "," "," "," "," "]
-     @board = board
+     @board = board == [" "," "," "," "," "," "," "," "," "]
      end
 
 end
