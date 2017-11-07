f8255cfd48e9329b49035bb3429430265864f4cc
Bperez4197 <bryceperez31@gmail.com>
2017-11-07 07:13:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,6 +68,18 @@ end
     end
   end
 
-
+def won?
+  combo = 0
+  while combo < WIN_COMBINATIONS.length
+    current_combo = WIN_COMBINATIONS[combo]
+    win1 = current_combo.all? {|position| @board[position] == "X"}
+    win2 = current_combo.all? {|position| @board[position] == "O"}
+    if win1 || win2
+      return current_combo
+    else
+    end
+    combo += 1
+  end
+end
 
 end
